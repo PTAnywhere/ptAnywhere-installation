4dda30663f0f7b48deaf7ec6bdd7f51d70810502
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-08-10 18:15:18 +0100

    Cleaning and documenting PT over VNC start script.

--- roles/packetTracer_multiple/files/startXvfb.sh
+++ roles/packetTracer_multiple/files/startXvfb.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 export PT6HOME=/opt/pt
+export DISPLAY=:1
 
+# Start Xvfb on virtual display 1
+Xvfb $DISPLAY -screen 0 1024x780x24&
+
+# Run Packet Tracer
 cd $PT6HOME
-Xvfb :1 -extension GLX -screen 0 1024x780x24& DISPLAY=:1 bin/PacketTracer6 saves/FORGE/ibookdemo611.pkt &
-x11vnc -forever -usepw -display :1
+bin/PacketTracer6 saves/FORGE/ibookdemo611.pkt &
+
+# Afterwards you could run other apps in this virtual display. E.g.:
+# DISPLAY=:1 firefox http://www.google.com &
+
+# VNC on virtual display
+x11vnc -forever -usepw -display $DISPLAY
 
 exit 0
